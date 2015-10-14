253824f34dbc72cc8f2d5bd6cf133578e5bf7cf7
Marc Lee <marc@adviso.com.au>
2015-10-15 00:31:50 +1100

    install lifx-api.

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
 var app = express();
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
